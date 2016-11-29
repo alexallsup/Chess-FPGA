b1e3a49f3656d532f77b16c1a558f0d5f55ed325
Kevin Wu <kwu135@usc.edu>
2016-11-29 07:38:17 -0800

    vga outputs a board, pawn, bishop art, selectedTiles (kind of)

--- vga_demo.v
+++ vga_demo.v
@@ -46,8 +46,90 @@ module vga_demo(ClkPort, vga_h_sync, vga_v_sync, vga_r, vga_g, vga_b, Sw0, Sw1, 
 	///////////////		VGA control starts here		/////////////////
 	/////////////////////////////////////////////////////////////////
 	reg [9:0] position;
-	reg [64:0] pieces;
+	//reg [64:0] pieces;
+	
+	reg [8:0] selectedTile = 50;
+	reg pawnArt [7:0][0:7] = {
+	8'b01111110,
+	8'b01111110,
+	8'b00111100,
+	8'b00011000,
+	8'b00011000,
+	8'b00011000,
+	8'b00111100,
+	8'b00011000};
+	
+	//00011000
+	//00111000
+	//01111110
+	//01111110
+	//00111100
+	//00011000
+	//00111100
+	//01111110
+	reg bishopArt [7:0][0:7] = {
+	8'b01111110,
+	8'b00111100,
+	8'b00011000,
+	8'b00111100,
+	8'b01111110,
+	8'b01111110,
+	8'b00011100,
+	8'b00011000};
+	
+	//00010000
+	//00111000
+	//01111110
+	//11111110
+	//11011100
+	//00011100
+	//00111110
+	//01111111
+	//reg [63:0] knightArt = 64'b0001000000111000011111101111111011011100000111000011111001111111;
+	//reg [63:0] knightArt = 64'b1111111001111100001110000011101101111111011111100001110000001000;
 
+	//01011010
+	//01011010
+	//01111110
+	//01111110
+	//00111100
+	//00111100
+	//01111110
+	//01111110
+	//reg [63:0] rookArt = 64'b0101101001011010011111100111111000111100001111000111111001111110;
+	//reg [63:0] rookArt = 64'b0111111001111110001111000011110001111110011111100101101001011010;
+
+	//00000000
+	//10011001
+	//10011001
+	//11011011
+	//11111111
+	//11111111
+	//11111111
+	//01111110
+	//reg [63:0] queenArt = 0000000010011001100110011101101111111111111111111111111101111110;
+	//reg [63:0] queenArt = 64'b0111111011111111111111111111111111011011100110011001100100000000;
+	
+	//00000000
+	//00011000
+	//00011000
+	//01111110
+	//01111110
+	//00011000
+	//00011000
+	//00011000
+	//reg [63:0] kingArt = 0000000000011000000110000111111001111110000110000001100000011000
+	//reg [63:0] kingArt = 64'b0001100000011000000110000111111001111110000110000001100000000000;
+	
+//	reg
+//	
+//	00011000
+//	00111100
+//	00011000
+//	00111100
+//	00111100
+//	00011000
+	
 	/*always @(posedge DIV_CLK[21])
 		begin
 			if(reset)
@@ -57,11 +139,84 @@ module vga_demo(ClkPort, vga_h_sync, vga_v_sync, vga_r, vga_g, vga_b, Sw0, Sw1, 
 			else if(btnU && ~btnD)
 				position<=position-2;	
 		end*/
+		
+	reg R = 0;
+	reg G = 0;
+	reg B = 0;
 
-	wire R = CounterY/60 + CounterX/60 %  2 == 0;
-	wire G = CounterY/60 + CounterX/60 %  2 == 0;
-	wire B = CounterY/60 + CounterX/60 %  2 == 0;
+	always @(CounterX or CounterY)
+	begin
+		R <= 0;
+		G <= 1;
+		B <= 0;
+		if (CounterY >= 40 && CounterY < 440) 
+		begin
+			if (CounterX >= 120 && CounterX < 520)
+			begin
+				/*if ((((CounterY+10)/50) == (selectedTile / 8) + 1) && (((CounterX+30)/50) == (selectedTile % 8) + 3) && 
+						(((CounterY+10) % 50 < 2) || ((CounterY+10) % 50 > 47) || ((CounterX+30) % 50 < 2) || ((CounterX+30) % 50 > 47)))
+				begin
+					R <= 1;
+					G <= 0;
+					B <= 0;
+				end*/
+				if ((((CounterY+10)/50) == (selectedTile / 8) + 1) && (((CounterX+30)/50) == (selectedTile % 8) + 3))
+				begin
+					if (((CounterY+10) % 50 >= 5) && ((CounterY+10) % 50 < 45) && ((CounterX+30) % 50 >= 5) && ((CounterX+30) % 50 < 45))
+					//(bishopArt[((((CounterY+5)%50)/5) * 8) + (((CounterX+25)%50)/5)] == 1))
+						begin
+							if (pawnArt[(((CounterY+5)%50)/5)][(((CounterX+25)%50)/5)] == 1)
+							begin
+								R <= 1;
+								G <= 0;
+								B <= 0;
+							end
+							else
+							begin
+								R <= ((((CounterY+10)/50) + (CounterX+30)/50) %  2 == 0);
+								G <= ((((CounterY+10)/50) + (CounterX+30)/50) %  2 == 0);
+								B <= ((((CounterY+10)/50) + (CounterX+30)/50) %  2 == 0);
+							end
+						end
+					else
+						begin
+							R <= ((((CounterY+10)/50) + (CounterX+30)/50) %  2 == 0);
+							G <= ((((CounterY+10)/50) + (CounterX+30)/50) %  2 == 0);
+							B <= ((((CounterY+10)/50) + (CounterX+30)/50) %  2 == 0);
+						end
+				end
+				else
+				begin
+						R <= ((((CounterY+10)/50) + (CounterX+30)/50) %  2 == 0);
+						G <= ((((CounterY+10)/50) + (CounterX+30)/50) %  2 == 0);
+						B <= ((((CounterY+10)/50) + (CounterX+30)/50) %  2 == 0);
+				end
+			end
+			else
+			begin
+				R <= 0;
+				G <= 0;
+				B <= 0;
+			end
+		end
+		else
+		begin
+			R <= 0;
+			G <= 0;
+			B <= 0;
+		end
+	end
 	
+	
+   //wire R = ((CounterY >= 40 && CounterY < 440) && (CounterX >= 120 && CounterX < 520) && ((((CounterY+10)/50) + (CounterX+30)/50) %  2 == 0));
+	//wire G = ((CounterY >= 40 && CounterY < 440) && (CounterX >= 120 && CounterX < 520) && ((((CounterY+10)/50) + (CounterX+30)/50) %  2 == 0));
+   //wire B = ((CounterY >= 40 && CounterY < 440) && (CounterX >= 120 && CounterX < 520) && ((((CounterY+10)/50) + (CounterX+30)/50) %  2 == 0));
+
+	//wire G = ((CounterY >= 40 && CounterY < 440) && (CounterX >= 120 && CounterX < 520) && ((((CounterY-40)/50) + ((CounterX-120)/50)) %  2 == 0));
+   //wire B = ((CounterY >= 40 && CounterY < 440) && (CounterX >= 120 && CounterX < 520) && ((((CounterY-40)/50) + ((CounterX-120)/50)) %  2 == 0));
+
+   //wire G = ((CounterY >= 40 && CounterY <= 440) && (CounterX >= 120 && CounterX <= 520) && ((((CounterY-40)/50) + ((CounterX-120)/50)) %  2 == 0));
+   //wire B = ((CounterY >= 40 && CounterY <= 440) && (CounterX >= 120 && CounterX <= 520) && ((((CounterY-40)/50) + ((CounterX-120)/50)) %  2 == 0));
 	/*if (piece[CounterY/60 * 8 + CounterX/60] == 1) {
 		R = 1;
 		B = 2;
